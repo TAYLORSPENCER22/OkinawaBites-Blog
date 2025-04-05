23d1dd72370e4325b46376af01cade40b90ea88a
TaylorSpencer22 <taylorspencer522@gmail.com>
2025-04-04 22:36:30 -0700

    it works

--- client/src/Header.js
+++ client/src/Header.js
@@ -26,6 +26,7 @@ export default function Header() {
         method: 'POST',
       }).then(() => {
         setUserInfo(null); //clear username 
+        setSideBarVisible(false); //if clicked from mobile 
         navigate('/'); //nav
       })
     };
@@ -90,7 +91,7 @@ export default function Header() {
               class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
         </svg>Logout</button>
-              <Link to ="/create" className="createNewPost nav.new"> 
+              <Link to ="/create" className="createNewPost nav.new" onClick={() => setSideBarVisible(false)}> 
               <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M208,32H48A16,16,0,0,0,32,48V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V48A16,16,0,0,0,208,32Zm0,176H48V48H208V208Zm-32-80a8,8,0,0,1-8,8H136v32a8,8,0,0,1-16,0V136H88a8,8,0,0,1,0-16h32V88a8,8,0,0,1,16,0v32h32A8,8,0,0,1,176,128Z"></path></svg>
               Create new post</Link>
               
@@ -99,10 +100,10 @@ export default function Header() {
           {!username && (
             <>
             
-              <Link to="/login" className="loginHeader nav.new">
+              <Link to="/login" className="loginHeader nav.new" onClick={() => setSideBarVisible(false)}>
               <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M141.66,133.66l-40,40a8,8,0,0,1-11.32-11.32L116.69,136H24a8,8,0,0,1,0-16h92.69L90.34,93.66a8,8,0,0,1,11.32-11.32l40,40A8,8,0,0,1,141.66,133.66ZM200,32H136a8,8,0,0,0,0,16h56V208H136a8,8,0,0,0,0,16h64a8,8,0,0,0,8-8V40A8,8,0,0,0,200,32Z"></path></svg>
                 Sign In</Link>
-              <Link to="/register" className="registerHeader">Register</Link>
+              <Link to="/register" className="registerHeader" onClick={() => setSideBarVisible(false)}>Register</Link>
               
             </>
           )}
