3db33568e722fff4acebb3f947d62c92fcc7463f
TaylorSpencer22 <taylorspencer522@gmail.com>
2025-04-07 23:20:22 -0700

    --

--- client/src/Header.js
+++ client/src/Header.js
@@ -74,7 +74,7 @@ export default function Header() {
          
         </nav>
 
-{/* separate navBar for smaller / mobile views */}
+{/* separate navBar for / mobile views */}
        
       
        <svg onClick={showSideBar} className="listBtn" xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="#F6F6F6" viewBox="0 0 256 256"><path d="M224,128a8,8,0,0,1-8,8H40a8,8,0,0,1,0-16H216A8,8,0,0,1,224,128ZM40,72H216a8,8,0,0,0,0-16H40a8,8,0,0,0,0,16ZM216,184H40a8,8,0,0,0,0,16H216a8,8,0,0,0,0-16Z"></path></svg>
